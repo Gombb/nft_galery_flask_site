fa56cbb8ed5645ec167b4d03b882fe3f70319d9b
Gombb <primegombb@protonmail.com>
2021-03-26 21:08:27 +0100

    refactor: function names in app.py

--- app.py
+++ app.py
@@ -8,16 +8,22 @@ def route_index():
     return render_template("index.html")
 
 
+@app.route("/detailed/artist/artist<artist_id>")
+def detailed_artist_view(artist_id):
+    return render_template("artist_detailed.html")
+
+
 @app.route("/discover")
 def route_discover():
     nfts = nft_cards.get_nft_list()
     return render_template("discover.html", nfts=nfts)
 
 
-@app.route("/detailed/<image_id>")
-def detailed_view(image_id):
+@app.route("/detailed/image/<image_id>")
+def detailed_image_view(image_id):
     nft_card = nft_cards.get_nft_by_id(image_id)
-    return render_template("detailed.html", nft_card=nft_card)
+    return render_template("image_detailed.html", nft_card=nft_card)
+
 
 if __name__ == '__main__':
     app.run()
