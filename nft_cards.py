6176c422ab8bc10b017be75b0f698788612efa79
Gombb <primegombb@protonmail.com>
2021-03-28 19:57:43 +0200

    refactor more refactor in nft_cards.py added debug mode for __init__.py

--- nft_cards.py
+++ nft_cards.py
@@ -22,21 +22,15 @@ img_link_3 = "https://craftinga.com/wp-content/uploads/2020/11/Trump-Witch.jpg"
 artist_1 = Artist(1, "Mr.M", 42)
 artist_2 = Artist(2, "Mr.D", 25)
 artist_dict = {artist_1.name: artist_1, artist_2.name: artist_2}
-nft_list = [Nft(1, "Neutrogena", artist_dict["Mr.D"],  0.555, img_link_1),
-            Nft(2, "Losange", artist_dict["Mr.M"], 1.555, img_link_2),
-            Nft(3, "LaKőr", artist_dict["Mr.M"], 520.21, img_link_3),
-            Nft(1, "Neutrogena", artist_dict["Mr.D"],  0.555, img_link_1),
-            Nft(2, "Losange", artist_dict["Mr.M"], 1.555, img_link_2),
-            Nft(3, "LaKőr", artist_dict["Mr.M"], 520.21, img_link_3),
-            Nft(1, "Neutrogena", artist_dict["Mr.D"],  0.555, img_link_1),
-            Nft(2, "Losange", artist_dict["Mr.M"], 1.555, img_link_2),
-            Nft(3, "LaKőr", artist_dict["Mr.M"], 520.21, img_link_3),
-            Nft(1, "Neutrogena", artist_dict["Mr.D"],  0.555, img_link_1),
-            Nft(2, "Losange", artist_dict["Mr.M"], 1.555, img_link_2),
-            Nft(3, "LaKőr", artist_dict["Mr.M"], 520.21, img_link_3),
-            Nft(1, "Neutrogena", artist_dict["Mr.D"],  0.555, img_link_1),
-            Nft(2, "Losange", artist_dict["Mr.M"], 1.555, img_link_2),
-            Nft(3, "LaKőr", artist_dict["Mr.M"], 520.21, img_link_3)]
+nft_1 = Nft(1, "Neutrogena", artist_dict["Mr.D"],  0.555, img_link_1)
+nft_2 = Nft(2, "Losange", artist_dict["Mr.M"], 1.555, img_link_2)
+nft_3 = Nft(3, "LaKőr", artist_dict["Mr.M"], 520.21, img_link_3)
+
+nft_list = []
+for i in range(10):
+    nft_list.append(nft_1)
+    nft_list.append(nft_2)
+    nft_list.append(nft_3)
 
 
 def init_nft():
