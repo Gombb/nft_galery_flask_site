cebc675f20e4e86bede4dd3af218680c55b27efa
Gombb <primegombb@protonmail.com>
2021-04-10 17:29:55 +0200

    refactor: generalize function names and variable names add basic structure for artist_detailed site

--- __init__.py
+++ __init__.py
@@ -8,20 +8,23 @@ def route_index():
     return render_template("index.html")
 
 
-@app.route("/detailed/artist/artist<artist_id>")
+@app.route("/detailed/artist/<artist_id>")
 def detailed_artist_view(artist_id):
-    return render_template("artist_detailed.html")
+    artist = product_service.get_artist(artist_id)
+    artist_products = product_service.get_products_for_artist_id(artist_id)
+    print(artist_products)
+    return render_template("artist_detailed.html", artist=artist, products=artist_products)
 
 
 @app.route("/discover")
 def route_discover():
-    products = product_service.get_nft_list()
+    products = product_service.get_product_list()
     return render_template("discover.html", nfts=products)
 
 
-@app.route("/detailed/image/<image_id>")
-def detailed_image_view(image_id):
-    product_card = product_service.get_nft_by_id(image_id)
+@app.route("/detailed/image/<product_id>")
+def detailed_image_view(product_id):
+    product_card = product_service.get_product_by_id(product_id)
     return render_template("image_detailed.html", nft_card=product_card)
 
 
--- product_service.py
+++ product_service.py
@@ -8,21 +8,24 @@ class Nft:
 
 
 class Artist:
-    def __init__(self, id, name, age):
+    def __init__(self, id, name, age, avatar):
         self.id = id
         self.name = name
         self.age = age
+        self.avatar = avatar
 
 
 img_link_1 = "https://i.pinimg.com/564x/1d/9b/57/1d9b5704645fcecb8d514fd8f798c136.jpg"
 img_link_2 = "https://storage.googleapis.com/gweb-uniblog-publish-prod/original_images/image-bgEuwDxel93-Pg-large_1.png"
 img_link_3 = "https://craftinga.com/wp-content/uploads/2020/11/Trump-Witch.jpg"
+avatar_1 = "https://pbs.twimg.com/profile_images/1346530588090634240/8wmsHX88_400x400.png"
 
 
-artist_1 = Artist(1, "Mr.M", 42)
-artist_2 = Artist(2, "Mr.D", 25)
+artist_1 = Artist(1, "Mr.M", 42, avatar_1)
+artist_2 = Artist(2, "Mr.D", 25, avatar_1)
+artist_list = [artist_1, artist_2]
 artist_dict = {artist_1.name: artist_1, artist_2.name: artist_2}
-nft_list = [Nft(1, "Neutrogena", artist_dict["Mr.D"],  0.555, img_link_1), Nft(2, "Losange", artist_dict["Mr.M"], 1.555, img_link_2),
+product_list = [Nft(1, "Neutrogena", artist_dict["Mr.D"],  0.555, img_link_1), Nft(2, "Losange", artist_dict["Mr.M"], 1.555, img_link_2),
             Nft(3, "LaKőr", artist_dict["Mr.M"], 520.21, img_link_3), Nft(1, "Neutrogena", artist_dict["Mr.D"],  0.555, img_link_1), Nft(2, "Losange", artist_dict["Mr.M"], 1.555, img_link_2),
             Nft(3, "LaKőr", artist_dict["Mr.M"], 520.21, img_link_3), Nft(1, "Neutrogena", artist_dict["Mr.D"],  0.555, img_link_1), Nft(2, "Losange", artist_dict["Mr.M"], 1.555, img_link_2),
             Nft(3, "LaKőr", artist_dict["Mr.M"], 520.21, img_link_3), Nft(1, "Neutrogena", artist_dict["Mr.D"],  0.555, img_link_1), Nft(2, "Losange", artist_dict["Mr.M"], 1.555, img_link_2),
@@ -34,11 +37,25 @@ def init_nft():
     pass
 
 
-def get_nft_by_id(id):
-    for nft in nft_list:
-        if nft.id == int(id):
-            return nft
+def get_artist(artist_id):
+    for artist in artist_list:
+        if artist.id == int(artist_id):
+            return artist
 
 
-def get_nft_list():
-    return nft_list
+def get_products_for_artist_id(artist_id):
+    result_list = []
+    for product in product_list:
+        if product.artist.id == int(artist_id):
+            result_list.append(product)
+    return result_list
+
+
+def get_product_by_id(product_id):
+    for product in product_list:
+        if product.id == int(product_id):
+            return product
+
+
+def get_product_list():
+    return product_list
