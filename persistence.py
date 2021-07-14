1c4e2fcbe16816de4006968540043881870f2b97
Gombb <primegombb@protonmail.com>
2021-07-14 16:12:29 +0200

    feat: add basic select * queries for persistence

--- persistence.py
+++ persistence.py
@@ -1,76 +1,29 @@
-import requests
+from psycopg2.extras import RealDictCursor
+import database_common
 
 
-class Nft:
-    def __init__(self, artist, contract_address, open_sea_token_id):
-        self.contract_address = contract_address
-        self.open_sea_token_id = open_sea_token_id
-        self.metadata = self.query_metadata_from_opensea()
-        self.last_price = price_in_ether(self.metadata["orders"][-1]["current_price"])
-        self.base_price = price_in_ether(self.metadata["orders"][0]["base_price"])
-        self.id = self.metadata["id"]
-        self.title = self.metadata["name"]
-        self.description = self.metadata["description"]
-        self.artist = artist
-        self.img_link = self.metadata["image_url"]
-
-    def query_metadata_from_opensea(self):
-        url = "https://api.opensea.io/api/v1/asset/"
-        url += self.contract_address + "/"
-        url += self.open_sea_token_id + "/"
-        curl_req = requests.get(url)
-        return curl_req.json()
+@database_common.connection_handler
+def get_artist_for_artist_id(cursor, artist_id):
+    cursor.execute(""" SELECT (name, bio, avatar_url)
+                        FROM artist 
+                        WHERE id = %(artist_id)s;""",
+                        {"artist_id", artist_id})
+    return cursor.fetchall()
 
 
-class Artist:
-    def __init__(self, id, name, age, avatar, bio):
-        self.id = id
-        self.name = name
-        self.age = age
-        self.avatar = avatar
-        self.bio = bio
+@database_common.connection_handler
+def get_all_product(cursor):
+    cursor.execute(""" SELECT * FROM collectible; """)
+    return cursor.fetchall()
 
 
-def price_in_ether(price_tag):
-    return round(float(price_tag) * 0.000000000000000001, 4)
+@database_common.connection_handler
+def get_all_artist(cursor):
+    cursor.execute("""
+                    SELECT *
+                    FROM artist;
+                    """)
+    return cursor.fetchall()
 
 
-def get_product_list():
-    return product_list
-
-
-def get_artist_list():
-    return artist_list
-
-
-contract_address1 = "0x495f947276749Ce646f68AC8c248420045cb7b5e"
-token_id1 = "63150080862586220144499677713436387260670121981595781099876257943183637348353"
-contract_address2 = "0x495f947276749Ce646f68AC8c248420045cb7b5e"
-token_id2 = "14756086664038233523160058507758541272568006944930919508280125615919402582017"
-contract_address3 = "0x495f947276749Ce646f68AC8c248420045cb7b5e"
-token_id3 = "14756086664038233523160058507758541272568006944930919508280125614819890954241"
-contract_address4 = "0xC2C747E0F7004F9E8817Db2ca4997657a7746928"
-token_id4 = "1131"
-
-test_img_link_1 = "https://i.pinimg.com/564x/1d/9b/57/1d9b5704645fcecb8d514fd8f798c136.jpg"
-test_img_link_2 = "https://storage.googleapis.com/gweb-uniblog-publish-prod/original_images/image-bgEuwDxel93-Pg-large_1.png"
-test_img_link_3 = "https://craftinga.com/wp-content/uploads/2020/11/Trump-Witch.jpg"
-avatar_1 = "https://pbs.twimg.com/profile_images/1346530588090634240/8wmsHX88_400x400.png"
-
-lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-artist_1 = Artist(1, "Mr.M", 42, avatar_1, lorem_ipsum)
-artist_2 = Artist(2, "Mr.D", 25, avatar_1, lorem_ipsum)
-artist_dict = {artist_1.name: artist_1, artist_2.name: artist_2}
-artist_list = [artist_1, artist_2]
-nft_1 = Nft(artist_dict["Mr.D"], contract_address1, token_id1)
-nft_2 = Nft(artist_dict["Mr.M"], contract_address2, token_id2)
-nft_3 = Nft(artist_dict["Mr.M"], contract_address3, token_id3)
-nft_4 = Nft(artist_dict["Mr.D"], contract_address4, token_id4)
-
-product_list = []
-for i in range(20):
-    product_list.append(nft_1)
-    product_list.append(nft_2)
-    product_list.append(nft_3)
-    product_list.append(nft_4)
-
+# lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
